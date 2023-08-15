f77d5118250819ffd69dc2a4c5b36cc518a7fe16
JoshBurke <joshDgreat37@gmail.com>
2023-08-15 01:25:32 -0700

    init WeSplit project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Josh Burke on 8/14/23.
+//  Copyright © 2023 joshburke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
